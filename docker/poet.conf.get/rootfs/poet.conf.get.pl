6a06fe0787e9ef5340afeb18551d3682ec05b367
Dave Baird <dave@zerofive.co.uk>
2020-12-26 21:12:54 +0100

    No log message supplied

--- docker/poet.conf.get/rootfs/poet.conf.get.pl
+++ docker/poet.conf.get/rootfs/poet.conf.get.pl
@@ -6,47 +6,43 @@ use Poet::Script qw($conf);
 use JSON;
 
 if ($ARGV[0] eq 'LIST') {
-    get_list();
+    get_list($ARGV[1]);
 }
 elsif ($ARGV[0] eq 'HASH') {
-    get_hash();
+    get_hash($ARGV[1]);
 }
 elsif ($ARGV[0] eq 'BOOLEAN') {
-    get_boolean();
+    get_boolean($ARGV[1]);
 }
 else {
-    get();
+    get($ARGV[0]);
 }
 
 exit(0);
 
 # -----
 sub get {
-    my $key = $ARGV[0] or die "Need a key";
+    my $key = $_[0] or die "Need a key for get()";
 
     my $value = $conf->get($key);
 
     return unless defined $value;
 
-    # defined($value) or die "No value defined for $key";
-
     print "$value\n";
 }
 
 sub get_list {
-    my $key = $ARGV[1] or die "Need a key";
+   my $key = $_[0] or die "Need a key for get_list()";
 
     my $arr = $conf->get_list($key);
 
     return unless defined $arr;
 
-    # defined($arr) or die "No value defined for $key";
-
     print "$_\n" for @$arr;
 }
 
 sub get_boolean {
-    my $key = $ARGV[1] or die "Need a key";
+    my $key = $_[0] or die "Need a key for get_boolean()";
 
     my $bool = $conf->getboolean($key); # 0 or 1
 
@@ -54,14 +50,12 @@ sub get_boolean {
 }
 
 sub get_hash {
-    my $key = $ARGV[1] or die "Need a key";
+    my $key = $_[0] or die "Need a key for get_hash()";
 
     my $hash = $conf->get_list($key);
 
-    return unless defined $hash;
+    # return unless defined $hash;
 
-    # defined($hash) or die "No value defined for $key";
-
-    print encode_json $hash;
+    print encode_json $hash; # will print the string 'null' if fed undef
 }
 
